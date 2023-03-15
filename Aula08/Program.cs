0975873727d17db98a2f51def96dba48b8b3be5d
Rafa-ListeM <rafaeliturci098@gmail.com>
2023-03-15 09:01:39 -0300

    .

--- Aula08/Program.cs
+++ Aula08/Program.cs
@@ -1,14 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Aula08;
 
-Console.WriteLine("Hello, World!");
-
-Produto p1 = new Produto(1, "Mouse sem fio", 59.90M);
-Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
-
-p1.setValor(69.90M);
-p1.setDescricao("Mouse Gamer Razor");
-Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
 
 Endereco e1 = new Endereco(
     "Vinicios de morais",
