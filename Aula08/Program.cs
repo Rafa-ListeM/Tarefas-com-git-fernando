b75504535ce6d97c13ccf9978bef06cb659ae62f
Rafa-ListeM <rafaeliturci098@gmail.com>
2023-03-15 08:30:38 -0300

    Corrigi program

--- Aula08/Program.cs
+++ Aula08/Program.cs
@@ -1,8 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Aula08;
+using ConsoleApp_Manha;
 
 Console.WriteLine("Hello, World!");
 
-Produto p1 = new Produto(1, "Mouse sem fio" , 59.90M);
-Console.WriteLine(p1.getDescricao() + " " + p1.getValor() );
+Produto p1 = new Produto(1, "Mouse sem fio", 59.90M);
+Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
+
+p1.setValor(69.90M);
+p1.setDescricao("Mouse Gamer Razor");
+Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
 
